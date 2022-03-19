0241a3429c8e1ba84dba927f302e62c29b3cbd6d
Legeor <legeor2004@mail.ru>
2022-03-19 03:29:09 +0300

    jora pobeda v 20

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ $(document).ready(function (){
       $('body').toggleClass('lock');
   });
 });
+
+ $(document).ready(function() {
+   $("a.header-link, a.header-logo").click(function() {
+     $("html, body").animate({
+       scrollTop: $($(this).attr("href")).offset().top - 150 + "px"
+
+     }, {
+       duration: 1000,
+       easing: "swing"
+     });
+   });
+ });
+
+
+ $(document).ready(function() {
+  $("a.header-link").click(function(){
+    $("a.header-link").removeClass('activ-list');
+    $(this).addClass('activ-list')
+  })
+  })
+
+
+  //$(window).scroll(function() {
+    // Get container scroll position
+    //var fromTop = $(this).scrollTop() + a.header-link.outerHeight() + 15
