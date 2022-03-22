2fdb613039979ba2b618f328ea50de1d63d7a5c0
Legeor <legeor2004@mail.ru>
2022-03-23 01:02:41 +0300

    jora-pobedav21

--- script.js
+++ script.js
@@ -3,29 +3,31 @@ $(document).ready(function (){
       $('.header-burger,.header-menu').toggleClass('active');
       $('body').toggleClass('lock');
   });
+  $('a.header-link').click(function () {
+    $('.header-burger,.header-menu').removeClass('active');
+    $('body').removeClass('lock'); 
+  });
 });
 
- $(document).ready(function() {
-   $("a.header-link, a.header-logo").click(function() {
-     $("html, body").animate({
-       scrollTop: $($(this).attr("href")).offset().top - 150 + "px"
+$(document).ready(function() {
+  $("a.header-link, a.header-logo").click(function() {
+    $("html, body").animate({
+      scrollTop: $($(this).attr("href")).offset().top - 150 + "px"
 
-     }, {
-       duration: 1000,
-       easing: "swing"
-     });
-   });
+    }, {
+      duration: 1000,
+      easing: "swing"
+    });
+  });
+});
+
+
+$(document).ready(function() {
+ $("a.header-link").click(function(){
+   $("a.header-link").removeClass('activ-list');
+   $(this).addClass('activ-list')
+ })
  });
 
 
- $(document).ready(function() {
-  $("a.header-link").click(function(){
-    $("a.header-link").removeClass('activ-list');
-    $(this).addClass('activ-list')
-  })
-  })
-
-
-  //$(window).scroll(function() {
-    // Get container scroll position
-    //var fromTop = $(this).scrollTop() + a.header-link.outerHeight() + 15
+  
